5311ef05efac08dd3ee87a091b5465b4cd81bcca
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2025-06-10 08:26:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (9.44%)

--- 0118-pascals-triangle/solution.cpp
+++ 0118-pascals-triangle/solution.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+
         vector<vector<int>> ans = {};
-        
-        //First element 1
         ans.push_back({1});
+        int middleElement = 0;
 
-        int middleElements = 0;
-
+        
         for(int i =1;i<numRows;i++){
-            vector<int> currentRow = {};
-
-            //first element
+            
+            vector<int> currentRow;
+            //first element 1
             currentRow.push_back(1);
-
+            
             //middle elements
-            for(int j =1;j<=middleElements;j++){
-                int element = ans[i-1][j] + ans[i-1][j-1];
-                currentRow.push_back(element);
+            for(int j = 1;j<=middleElement;j++){
+                int val = ans[i-1][j] + ans[i-1][j-1];
+                currentRow.push_back(val);
             }
 
             //last element
             currentRow.push_back(1);
-            middleElements++;
+
+            middleElement++;
             ans.push_back(currentRow);
         }
-
         return ans;
+        
     }
 };
