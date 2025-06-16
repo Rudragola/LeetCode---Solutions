ae11d512b3481c68c21e2f5ead2a732382a49a07
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2025-06-16 09:37:33 +0000

    Sync LeetCode submission Runtime - 35 ms (98.89%), Memory - 29.2 MB (28.60%)

---
+++ 0015-3sum/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin() , nums.end());
+        int n = nums.size();
+        vector<vector<int>> ans;
+
+        for(int i = 0;i<n-2;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int target = -nums[i];
+
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                int sum = nums[j]+nums[k];
+                if(sum == target ){
+                    ans.push_back({nums[i] , nums[j] , nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                    
+                }else if(sum<target){
+                    j++;
+                }else{
+                     k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
