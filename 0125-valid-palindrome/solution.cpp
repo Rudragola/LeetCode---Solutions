05ad4370959e6db6b4246a9a9d9d830eeddbcf97
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2025-07-05 10:03:39 +0000

    Sync LeetCode submission Runtime - 4 ms (14.21%), Memory - 9.7 MB (94.16%)

--- 0125-valid-palindrome/solution.cpp
+++ 0125-valid-palindrome/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-         /* string cleaned;
+        /* string cleaned;
         for(char c: s){
             if(isalnum(c)){
                 cleaned += tolower(c);
@@ -29,25 +29,15 @@ public:
         
     }
 
-    bool check(string& s , int left , int right){
-            
-            //if starting character is not alphanumeric move ahead
-            while(left<right && !isalnum(s[left])) left++;
+    bool check(string& s,int left , int right){
+        while(left<right && !isalnum(s[left])) left++;
 
-            //if ending character is not alphanumeric move backward
-            while(left<right && !isalnum(s[right])) right--;
-
-            
-            //base case
-            if(left >= right) return true;
-
-            //check 
-            if(tolower(s[left]) != tolower(s[right])) return false;
-
-            //recursion case
-            return check(s , left+1 , right-1);
-
-        }
-}; 
-
+        while(left<right && !isalnum(s[right])) right--;
         
+        if(left>=right) return true;
+
+        if(tolower(s[left]) != tolower(s[right])) return false;
+
+        return check(s , left+1 , right -1);
+    }
+};
