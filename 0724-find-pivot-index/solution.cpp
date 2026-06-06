6b46260c7d2f8687ea11b1d21182adfd6c02b778
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-06 06:53:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 35.9 MB (29.63%)

--- 0724-find-pivot-index/solution.cpp
+++ 0724-find-pivot-index/solution.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        int sum =0;
-        for(int i = 0;i<n;i++){
-            sum+=nums[i];
+        int n= nums.size();
+        int ts = 0;
+        int ls =0,rs = 0;
+        for(int i =0;i<n;i++){
+            ts += nums[i];
         }
 
-        int cs = 0;
-        for(int i = 0;i<n;i++){
-            int ls = cs;
-            int rs = sum - ls - nums[i];
-            if(ls == rs){return i;}
-            else{
-                cs+=nums[i];
+        for(int i =0;i<n;i++){
+            rs = ts - nums[i] - ls;
+
+            if(ls == rs){
+                return i;
             }
+
+            ls += nums[i];
         }
         return -1;
-        
     }
 };
