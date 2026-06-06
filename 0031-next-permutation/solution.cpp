e176b7e039608a745e1e350a0e50996c7ea21cb2
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-06 05:40:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.6 MB (98.74%)

---
+++ 0031-next-permutation/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int inx = -1;
+
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                inx = i;
+                break;
+            }
+        }
+        if(inx == -1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+
+        for(int i = n-1;i>inx;i--){
+            if(nums[i]>nums[inx]){
+                swap(nums[i],nums[inx]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+(inx+1) , nums.end());
+        
+    }
+};
