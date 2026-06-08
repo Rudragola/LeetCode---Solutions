741720f783c1e054fed2062314005b5806953ace
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-08 07:29:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (70.80%)

--- 0048-rotate-image/solution.cpp
+++ 0048-rotate-image/solution.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n =matrix.size();
-        
-        //TRANSPOSE
-        for(int i =0;i<n;i++){
-            for(int j =i;j<n;j++){
-                swap(matrix[i][j],matrix[j][i]);
-            }
-        }
-
-        //REVERSE
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n/2;j++){
-                swap(matrix[i][j],matrix[i][n-1-j]);
-            }
-        }
-
+        int n = matrix.size();
         for(int i =0;i<n;i++){
             for(int j =0;j<n;j++){
-                cout<<matrix[i][j];
+                //take extra n^2 space matrix[i][j] = matrix[j][n-1-i];
+                //transpose
+                if(j>i){
+                    swap(matrix[i][j] , matrix[j][i]);
+                }
+                
             }
+        }
+
+        for(int i =0;i<n;i++){
+            /* int j =0;
+            int k =n-1;
+            while(j<k){
+                swap(matrix[i][j] , matrix[i][k]);
+                j++;
+                k--;
+            } */
+
+            reverse(matrix[i].begin() , matrix[i].end());
         }
     }
 };
