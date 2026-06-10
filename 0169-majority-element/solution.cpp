8e33cee45eb33a1666e6d98c0fe817cf566e2fb7
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-10 06:58:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.3 MB (14.93%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int n = nums.size();
         int freq = 0;
         int ele = nums[0];
-        int n = nums.size();
 
-        for(int i=0;i<n;i++){
-            if(nums[i] == ele){
+        for(int i = 0;i<n;i++){
+            if(ele == nums[i]){
                 freq++;
             }else if(freq == 0){
                 ele = nums[i];
                 freq++;
-            }
-            else{
+            }else{
                 freq--;
             }
         }
-        
         return ele;
-        
     }
 };
