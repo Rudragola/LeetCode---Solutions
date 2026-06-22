0a2f28f03bd832fc0569897d2e8ded08cd3e3ec0
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-22 13:47:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (19.17%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
 
-    int firstOccur(vector<int>& nums, int target , int n){
-        int st = 0;
-        int end = n-1;
+    int firstOccur(vector<int>& nums , int n , int target ){
+        int low = 0 ;
+        int high = n-1;
         int ans = -1;
 
-        while(st <= end){
-            int mid = st + (end - st)/2;
-
+        while(low <= high){
+            int mid = (low+high) /2;
             if(nums[mid] == target){
                 ans = mid;
-                //go left
-                end = mid - 1;
-            }else if(nums[mid]<target){
-                //go right
-                st = mid + 1;
+                high = mid-1;
+            }else if(nums[mid] > target){
+                high = mid-1;
             }else{
-                //go left
-                end = mid - 1;
+                low = mid +1;
             }
         }
         return ans;
     }
 
-    int lastOccur(vector<int>& nums, int target , int n){
-        int st = 0;
-        int end = n-1;
+    int lastOccur(vector<int>& nums , int n , int target ){
+        int low = 0 ;
+        int high = n-1;
         int ans = -1;
 
-        while(st <= end){
-            int mid = st + (end - st)/2;
-
+        while(low <= high){
+            int mid = (low+high) /2;
             if(nums[mid] == target){
                 ans = mid;
-                //go right
-                st = mid + 1;
-            }else if(nums[mid]<target){
-                //go right
-                st = mid + 1;
+                low = mid+1;
+            }else if(nums[mid] > target){
+                high = mid-1;
             }else{
-                //go left
-                end = mid - 1;
+                low = mid +1;
             }
         }
         return ans;
     }
-    
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int fo = firstOccur(nums , target , n);
-        int lo = lastOccur(nums , target , n);
-        
-        return {fo,lo};
+        return {firstOccur(nums , nums.size() , target) ,lastOccur(nums , nums.size() , target) };
     }
 };
