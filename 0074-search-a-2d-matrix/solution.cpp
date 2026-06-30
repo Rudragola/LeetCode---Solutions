9045c9e5aa46316562ee29e83a42fab9b87a2211
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-06-30 06:15:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.4 MB (7.04%)

--- 0074-search-a-2d-matrix/solution.cpp
+++ 0074-search-a-2d-matrix/solution.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-
-    bool searchInRow(vector<vector<int>>& matrix, int target , int row){
-        int n = matrix[0].size();
-        int st = 0;
-        int end = n -1;
-
-        while(st<=end){
-            int mid = st + (end - st)/2;
-
-            if(matrix[row][mid] == target){
-                return true;
-            }
-            else if(target > matrix[row][mid]){
-                //go right
-                st = mid + 1;
-            }
-            else{
-                //go left
-                end = mid - 1;
-            }
-        }
-        return false;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size(); //row
-        int m = matrix[0].size(); //column
-        int startRow = 0;
-        int endRow = n - 1;
-        
-        while(startRow <= endRow){
-            int midRow = startRow + (endRow - startRow)/2;
-
-            if(target >= matrix[midRow][0] && target <= matrix[midRow][m-1]){
-                return searchInRow(matrix , target ,midRow);
-            }
-            else if(target >= matrix[midRow][m - 1]){
-                //down --> right
-                startRow = midRow + 1;
+        int n = matrix.size();
+        if(n == 0) return false;
+        int m = matrix[0].size();
+        if(m == 0 ) return false;
+        int low = 0;
+        int high = n*m - 1;
+        while(low <= high){
+            int mid = low + (high - low )/2;
+            int row = mid / m;
+            int col = mid % m;
+            if(matrix[row][col] == target){
+                return true;
+            }else if(matrix[row][col] > target){
+                high = mid -1;
             }else{
-                //up --> left
-                endRow = midRow - 1;
+                low = mid + 1;
             }
         }
         return false;
