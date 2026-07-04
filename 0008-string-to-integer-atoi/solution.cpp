5b7fa5a3715892bbe5b7433c6504473f2012e8f6
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-07-04 04:06:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (19.47%)

---
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.length();
+        int i = 0;
+        int sign = 1;
+        long long num = 0;
+        while(i<n && s[i] == ' ')i++;
+
+        if(i<n && (s[i] == '+' || s[i] == '-')){
+            if(s[i] == '-'){
+                sign = -1;
+            }
+            i++;
+        }
+
+        while(i<n && (s[i] >= '0' && s[i] <= '9')){
+            num = num * 10 + (s[i] - '0');
+
+            if (sign * num >= INT_MAX) return INT_MAX;
+            if (sign * num <= INT_MIN) return INT_MIN;
+
+            i++;
+        }
+
+        return (int)(sign*num);
+
+    }
+};
