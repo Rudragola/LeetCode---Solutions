1c2f459e44b3bc3a79b987c02d9e13dcb3c73731
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-07-07 10:15:27 +0000

    Sync LeetCode submission Runtime - 12 ms (41.65%), Memory - 11.9 MB (22.90%)

--- 0141-linked-list-cycle/solution.cpp
+++ 0141-linked-list-cycle/solution.cpp
@@ -12,12 +12,12 @@ public:
         ListNode* fast = head;
         ListNode* slow = head;
 
-        while(fast != NULL && fast->next != NULL){
+        while( fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
 
-            if(fast == slow){
-            return true;
+            if (slow == fast){
+                return true;
             }
         }
         return false;
