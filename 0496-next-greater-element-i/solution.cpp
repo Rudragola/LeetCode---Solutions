ca21c1da91bb07178e22f7ca81d916700dd71826
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-07-22 07:40:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (76.38%)

---
+++ 0496-next-greater-element-i/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums2.size();
+        unordered_map<int,int> mpp;
+        vector<int> ans;
+        stack<int> st;
+
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty() && st.top() <= nums2[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                mpp[nums2[i]] = -1;
+            }else{
+                mpp[nums2[i]] = st.top();
+            }
+            st.push(nums2[i]);
+        }
+
+        for(int i =0 ;i<nums1.size();i++){
+            ans.push_back(mpp[nums1[i]]);
+        }
+        return ans;
+    }
+};
