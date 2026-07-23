1b2d7ca937a8e2f353545137ec44afa043bbb66b
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-07-23 09:23:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.6 MB (79.12%)

---
+++ 0735-asteroid-collision/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            if(nums[i] > 0){
+                ans.push_back(nums[i]);
+            }else{
+                while(!ans.empty() && ans.back() > 0 && ans.back() < abs(nums[i])){
+                    ans.pop_back();
+                }
+                if(!ans.empty() && abs(nums[i]) == ans.back()){
+                    ans.pop_back();
+                }else if(ans.empty() || ans.back() < 0){
+                    ans.push_back(nums[i]);
+                }
+            }
+        }
+        return ans;
+    }
+};
