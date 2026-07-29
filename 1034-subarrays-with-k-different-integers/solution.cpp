5d27203a942e85e63e9705f8c4a962f79edc6c71
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-07-29 05:49:14 +0000

    Sync LeetCode submission Runtime - 81 ms (63.35%), Memory - 56.1 MB (57.53%)

---
+++ 1034-subarrays-with-k-different-integers/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int atMost(vector<int>& nums, int k){
+        int n = nums.size();
+        int  l = 0;
+        int r = 0;
+        int ans = 0;
+        unordered_map<int,int> mpp;
+        while(r<n){
+            mpp[nums[r]]++;
+            while(mpp.size() > k){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]] == 0){
+                    mpp.erase(nums[l]);
+                }
+                l++;
+            }
+
+            ans += r-l+1;
+            r++;
+        }
+        return ans;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums,k) - atMost(nums,k-1);
+    }
+};
