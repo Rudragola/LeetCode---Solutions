d2fab292be0c8a4c321e3ae5653a1585d6a71c81
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-08-04 15:31:58 +0000

    Sync LeetCode submission Runtime - 3 ms (63.71%), Memory - 11.7 MB (74.98%)

--- 0766-flatten-a-multilevel-doubly-linked-list/solution.cpp
+++ 0766-flatten-a-multilevel-doubly-linked-list/solution.cpp
@@ -13,27 +13,30 @@ class Solution {
 public:
     Node* flatten(Node* head) {
         Node* cur = head;
-        while(cur != NULL){
-            if(cur->child != NULL){
-                //flatten
-                Node* next =cur->next;
+
+        while (cur != NULL) {
+            // flatten
+            if (cur->child != NULL) {
+                Node* nextt = cur->next;
                 cur->next = flatten(cur->child);
                 cur->next->prev = cur;
                 cur->child = NULL;
 
-                //tail
-                while(cur->next != NULL){
+                // TAIL
+                while (cur->next != NULL) {
                     cur = cur->next;
                 }
 
-                //attach
-                if(next != NULL){
-                    cur->next = next;
-                    cur->next->prev = cur;
+                // atach
+                if (nextt != NULL) {
+                    cur->next = nextt;
+                    nextt->prev = cur;
                 }
             }
+
             cur = cur->next;
         }
+
         return head;
     }
 };
