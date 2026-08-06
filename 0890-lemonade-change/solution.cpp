c9829c712642bfbe21dbec9e3246ca79ef61ffb4
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-08-06 15:04:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 87.4 MB (11.16%)

---
+++ 0890-lemonade-change/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int n = bills.size();
+        int count5 = 0;
+        int count10 = 0;
+        int count20 = 0;
+        for(int i = 0;i<n;i++){
+            if(bills[i] == 5){
+                count5++;
+            }else if(bills[i] == 10){
+                if(count5 == 0)return false;
+                count5--;
+                count10++;
+            }else{
+                if(count10 && count5){
+                    count5--;
+                    count10--;
+                }else if(count5 >= 3){
+                    count5 -= 3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+};
