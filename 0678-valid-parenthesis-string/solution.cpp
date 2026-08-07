55403bbd9a5cd9318a35093f04052ba0dda78127
Rudra Kumar Gola <rudrakumargola855@gmail.com>
2026-08-07 12:01:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (93.19%)

---
+++ 0678-valid-parenthesis-string/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int n = s.length();
+        int minOpen = 0;
+        int maxOpen = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                minOpen++;
+                maxOpen++;
+            } else if (s[i] == ')') {
+                minOpen--;
+                maxOpen--;
+
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+            if (maxOpen < 0)
+                return false;
+            if (minOpen < 0)
+                minOpen = 0;
+        }
+        return minOpen == 0;
+    }
+};
